1383ed737f4898c8a6cccdba98dc4b26e67c0b98
Martin Ergang <xergang@fi.muni.cz>
2023-12-18 12:38:12 +0100

    Add day 17 part 2

--- day17/part2.cpp
+++ day17/part2.cpp
@@ -7,10 +7,12 @@
 #include <set>
 #include <vector>
 
+#define MC 10
+
 typedef std::vector<std::string> city_map;
 
 struct node {
-    node(int x, int y) : x(x), y(y), up(3), down(3), left(3), right(3) {}
+    node(int x, int y) : x(x), y(y), up(MC), down(MC), left(MC), right(MC) {}
 
     node(int x, int y, int up, int down, int left, int right)
         : x(x), y(y), up(up), down(down), left(left), right(right) {}
@@ -32,20 +34,50 @@ void load_map(city_map& m, std::fstream& fs) {
 std::set<node> get_adj(city_map& m, node& n) {
     std::set<node> res;
 
-    if (n.x > 0 && n.left > 0 && n.right == 3) {
-        res.insert({n.x - 1, n.y, 3, 3, n.left - 1, 3});
+    if (n.left > 6 && n.left < MC) {
+        if (n.x > 0 && n.right == MC)
+            return {{n.x - 1, n.y, MC, MC, n.left - 1, MC}};
+        else
+            return {};
     }
 
-    if (n.x < m[0].size() - 1 && n.right > 0 && n.left == 3) {
-        res.insert({n.x + 1, n.y, 3, 3, 3, n.right - 1});
+    if (n.right > 6 && n.right < MC) {
+        if (n.x < m[0].size() - 1 && n.left == MC)
+            return {{n.x + 1, n.y, MC, MC, MC, n.right - 1}};
+        else
+            return {};
     }
 
-    if (n.y > 0 && n.up > 0 && n.down == 3) {
-        res.insert({n.x, n.y - 1, n.up - 1, 3, 3, 3});
+    if (n.up > 6 && n.up < MC) {
+        if (n.y > 0 && n.down == MC)
+            return {{n.x, n.y - 1, n.up - 1, MC, MC, MC}};
+        else
+            return {};
     }
 
-    if (n.y < m.size() - 1 && n.down > 0 && n.up == 3) {
-        res.insert({n.x, n.y + 1, 3, n.down - 1, 3, 3});
+    if (n.down > 6 && n.down < MC) {
+        if (n.y < m.size() - 1 && n.up == MC)
+            return {{n.x, n.y + 1, MC, n.down - 1, MC, MC}};
+        else
+            return {};
+    }
+
+    //-------------------------------------------------------------
+
+    if (n.x > 0 && n.left > 0 && n.right == MC) {
+        res.insert({n.x - 1, n.y, MC, MC, n.left - 1, MC});
+    }
+
+    if (n.x < m[0].size() - 1 && n.right > 0 && n.left == MC) {
+        res.insert({n.x + 1, n.y, MC, MC, MC, n.right - 1});
+    }
+
+    if (n.y > 0 && n.up > 0 && n.down == MC) {
+        res.insert({n.x, n.y - 1, n.up - 1, MC, MC, MC});
+    }
+
+    if (n.y < m.size() - 1 && n.down > 0 && n.up == MC) {
+        res.insert({n.x, n.y + 1, MC, n.down - 1, MC, MC});
     }
 
     return res;
@@ -81,14 +113,14 @@ int find_path(city_map& m, int x_start, int y_start, int x_end, int y_end) {
 
     int res = std::numeric_limits<int>::max();
     for (auto& [n, d] : distances) {
-        if (n.x == x_end && n.y == y_end) res = std::min(res, d);
+        if (n.x == x_end && n.y == y_end && (n.right < 7 || n.down < 7)) res = std::min(res, d);
     }
 
     return res;
 }
 
 int main() {
-    std::fstream input("input_small.txt");
+    std::fstream input("input.txt");
 
     city_map m;
     load_map(m, input);
